f4d9a8268581b27ad6eee1a6a31a2e456c792c7f
Steve Bruell <bruell@cs.uiowa.edu>
2024-07-17 14:57:28 -0400

    Added LoadingStateServiceTests.cs

---
+++ Service Tests/LoadingStateServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using DBExplorerBlazor.Services;
+
+namespace Service;
+
+public class LoadingStateServiceTests
+{
+    [Fact]
+    public void ShowLoadingPanel_RaisesOnLoadingStateChangedWithTrue()
+    {
+        // Arrange
+        var service = new LoadingStateService();
+        bool? eventRaisedWith = null;
+        service.OnLoadingStateChanged += (isLoading) => eventRaisedWith = isLoading;
+
+        // Act
+        service.ShowLoadingPanel();
+
+        // Assert
+        Assert.True(eventRaisedWith.HasValue && eventRaisedWith.Value);
+    }
+
+    [Fact]
+    public void HideLoadingPanel_RaisesOnLoadingStateChangedWithFalse()
+    {
+        // Arrange
+        var service = new LoadingStateService();
+        bool? eventRaisedWith = null;
+        service.OnLoadingStateChanged += (isLoading) => eventRaisedWith = isLoading;
+
+        // Act
+        service.HideLoadingPanel();
+
+        // Assert
+        Assert.True(eventRaisedWith.HasValue && !eventRaisedWith.Value);
+    }
+}
