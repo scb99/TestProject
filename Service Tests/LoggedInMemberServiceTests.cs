ca37cd922cf2adc907c06456f10f2ed1feb4b92a
Steve Bruell <bruell@cs.uiowa.edu>
2024-07-17 15:08:32 -0400

    Added LoggedInMeberServiceTests.cs

---
+++ Service Tests/LoggedInMemberServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Services;
+
+namespace Service;
+
+public class LoggedInMemberServiceTests
+{
+    [Fact]
+    public void MemberGreeting_GetSet_ReturnsCorrectValue()
+    {
+        // Arrange
+        var service = new LoggedInMemberService();
+        var expectedGreeting = "Hello, User!";
+
+        // Act
+        service.MemberGreeting = expectedGreeting;
+        var actualGreeting = service.MemberGreeting;
+
+        // Assert
+        Assert.Equal(expectedGreeting, actualGreeting);
+    }
+
+    [Fact]
+    public void MemberRole_GetSet_ReturnsCorrectValue()
+    {
+        // Arrange
+        var service = new LoggedInMemberService();
+        var expectedRole = "Administrator";
+
+        // Act
+        service.MemberRole = expectedRole;
+        var actualRole = service.MemberRole;
+
+        // Assert
+        Assert.Equal(expectedRole, actualRole);
+    }
+
+    [Fact]
+    public void MemberUserID_GetSet_ReturnsCorrectValue()
+    {
+        // Arrange
+        var service = new LoggedInMemberService();
+        var expectedUserID = 123;
+
+        // Act
+        service.MemberUserID = expectedUserID;
+        var actualUserID = service.MemberUserID;
+
+        // Assert
+        Assert.Equal(expectedUserID, actualUserID);
+    }
+}
