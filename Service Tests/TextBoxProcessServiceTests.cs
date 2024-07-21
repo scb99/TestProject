1464bc8a8fb88def28f02c7b69002cb3266a570a
Steve Bruell <bruell@cs.uiowa.edu>
2024-07-21 09:44:46 -0400

    Modified TextBoxProcessServiceTests.cs

--- Service Tests/TextBoxProcessServiceTests.cs
+++ Service Tests/TextBoxProcessServiceTests.cs
@@ -7,49 +7,29 @@ namespace Service;
 
 public class TextBoxProcessServiceTests
 {
-    private readonly Mock<ILoggerService> _loggerServiceMock = new();
-    private readonly Mock<IMemberFilteringService> _memberFilteringServiceMock = new();
-    private readonly TextBoxProcessService _textBoxProcessService;
-
-    public TextBoxProcessServiceTests()
-    {
-        _textBoxProcessService = new TextBoxProcessService(_loggerServiceMock.Object, _memberFilteringServiceMock.Object);
-    }
-
     [Fact]
-    public async Task ProcessTextBoxInputChangeAsync_ReturnsFilteredMembers_OnSuccess()
+    public async Task ProcessTextBoxInputChangeAsync_ReturnsFilteredMembers()
     {
         // Arrange
-        var inputValue = "test";
-        var filterCriteria = "name";
+        var mockMemberFilteringService = new Mock<IMemberFilteringService>();
+        var testValue = "test";
+        var testFilterCriteria = "criteria";
         var expectedMembers = new List<MemberEntity>
         {
             new() { /* Initialize properties as needed */ },
             new() { /* Initialize properties as needed */ }
         };
-        _memberFilteringServiceMock.Setup(service => service.FilterMembersAsync(inputValue, filterCriteria)).ReturnsAsync(expectedMembers);
+
+        mockMemberFilteringService.Setup(service => service.FilterMembersAsync(testValue, testFilterCriteria))
+            .ReturnsAsync(expectedMembers);
+
+        var textBoxProcessService = new TextBoxProcessService(mockMemberFilteringService.Object);
 
         // Act
-        var result = await _textBoxProcessService.ProcessTextBoxInputChangeAsync(inputValue, filterCriteria);
+        var result = await textBoxProcessService.ProcessTextBoxInputChangeAsync(testValue, testFilterCriteria);
 
         // Assert
         Assert.Equal(expectedMembers.Count, result.Count);
-    }
-
-    [Fact]
-    public async Task ProcessTextBoxInputChangeAsync_ReturnsEmptyList_OnException()
-    {
-        // Arrange
-        var inputValue = "test";
-        var filterCriteria = "name";
-        _memberFilteringServiceMock.Setup(service => service.FilterMembersAsync(inputValue, filterCriteria)).ThrowsAsync(new Exception("Test exception"));
-
-        // Act
-        var result = await _textBoxProcessService.ProcessTextBoxInputChangeAsync(inputValue, filterCriteria);
-
-        // Assert
-        Assert.Empty(result);
-        _loggerServiceMock.Verify(l => l.LogExceptionAsync(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+        mockMemberFilteringService.Verify(service => service.FilterMembersAsync(testValue, testFilterCriteria), Times.Once);
     }
 }
-
