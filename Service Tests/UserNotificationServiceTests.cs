ce139a674e42662d8df746e3d6f5efec783ca74f
Steve Bruell <bruell@cs.uiowa.edu>
2024-07-21 09:56:26 -0400

    Added UserNotificationServiceTests.cs

---
+++ Service Tests/UserNotificationServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using DBExplorerBlazor.Interfaces;
+using DBExplorerBlazor.Services;
+using Moq;
+
+namespace Service;
+
+public class UserNotificationServiceTests
+{
+    [Fact]
+    public async Task AlertAsync_CallsAlertServiceWithCorrectMessage()
+    {
+        // Arrange
+        var mockAlertService = new Mock<IAlertService>();
+        var mockLoggerService = new Mock<ILoggerService>();
+        var userNotificationService = new UserNotificationService(mockAlertService.Object, mockLoggerService.Object);
+        string testMessage = "Test Alert Message";
+
+        // Act
+        await userNotificationService.AlertAsync(testMessage);
+
+        // Assert
+        mockAlertService.Verify(service => service.AlertUsingFallingMessageBoxAsync(testMessage), Times.Once);
+    }
+
+    [Fact]
+    public async Task LogAsync_CallsLoggerServiceWithCorrectMessage()
+    {
+        // Arrange
+        var mockAlertService = new Mock<IAlertService>();
+        var mockLoggerService = new Mock<ILoggerService>();
+        var userNotificationService = new UserNotificationService(mockAlertService.Object, mockLoggerService.Object);
+        string testMessage = "Test Log Message";
+
+        // Act
+        await userNotificationService.LogAsync(testMessage);
+
+        // Assert
+        mockLoggerService.Verify(service => service.LogResultAsync(testMessage), Times.Once);
+    }
+}
