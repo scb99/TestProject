fea45d4cb447e3193b5e78d706754a4363e09481
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-08 15:25:41 -0400

    Tests for MembersPage and AuthorizationService

---
+++ Service Tests/Members Page/AuthorizationServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using DBExplorerBlazor.Interfaces;
+using DBExplorerBlazor.Services;
+using Moq;
+
+namespace Authorization;
+
+public class AuthorizationServiceTests
+{
+    [Theory]
+    [InlineData("SuperUser", true)]
+    [InlineData("Admin", true)]
+    [InlineData("RegularUser", false)]
+    [InlineData("Guest", false)]
+    public void IsAuthorized_ReturnsExpectedResult(string role, bool expectedResult)
+    {
+        // Arrange
+        var mockLoggedInMemberService = new Mock<ICrossCuttingLoggedInMemberService>();
+        mockLoggedInMemberService.Setup(service => service.MemberRole).Returns(role);
+
+        var authorizationService = new AuthorizationService(mockLoggedInMemberService.Object);
+
+        // Act
+        var result = authorizationService.IsAuthorized();
+
+        // Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
