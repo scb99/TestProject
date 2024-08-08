3cb0be18b0d9614ea191319029647caad9250034
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-08 15:47:21 -0400

    Added tests for MailChimpMembersPageTests.cs

---
+++ Pages Tests/MailChimpMembersPageTests.cs
@@ -0,0 +1,38 @@
+﻿using DBExplorerBlazor.Pages;
+using Microsoft.AspNetCore.Components;
+using Moq;
+
+namespace Pages;
+
+public class MailChimpMembersPageTests
+{
+    [Fact]
+    public void OnYesLogoutButtonClicked_SetsCanShowMailChimpNamesToTrue()
+    {
+        // Arrange
+        var page = new MailChimpMembersPage();
+
+        // Act
+        page.OnYesLogoutButtonClicked();
+
+        // Assert
+        Assert.True(page.CanShowMailChimpNames);
+    }
+
+    //[Fact]
+    //public void OnNoLogoutButtonClicked_NavigatesToWelcomePage()
+    //{
+    //    // Arrange
+    //    var mockNavigationManager = new Mock<NavigationManager>();
+    //    var page = new MailChimpMembersPage
+    //    {
+    //        NavigationManager = mockNavigationManager.Object
+    //    };
+
+    //    // Act
+    //    page.OnNoLogoutButtonClicked();
+
+    //    // Assert
+    //    mockNavigationManager.Verify(nm => nm.NavigateTo("WelcomePage", It.IsAny<bool>()), Times.Once);
+    //}
+}
