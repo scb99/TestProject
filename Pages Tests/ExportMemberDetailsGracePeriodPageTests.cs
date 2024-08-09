2f4a2f9b3d10993b7f635ad1f1a2d5f4c3f33010
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 08:46:26 -0400

    Added ExportMembersDetailsGracePeriodPage tests

---
+++ Pages Tests/ExportMemberDetailsGracePeriodPageTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class ExportMemberDetailsGracePeriodPageTests
+{
+    [Fact]
+    public void CanShowListOfMemberDetails_ShouldReturnFalse_WhenGracePeriodIsMinusOne()
+    {
+        // Arrange
+        var page = new ExportMemberDetailsGracePeriodPage();
+
+        // Act
+        var result = page.CanShowListOfMemberDetails;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowListOfMemberDetails_ShouldReturnTrue_WhenGracePeriodIsSet()
+    {
+        // Arrange
+        var page = new ExportMemberDetailsGracePeriodPage
+        {
+            GracePeriod = 10
+        };
+
+        // Act
+        var result = page.CanShowListOfMemberDetails;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowListOfMemberDetails_ShouldReturnTrue_WhenGracePeriodIsZero()
+    {
+        // Arrange
+        var page = new ExportMemberDetailsGracePeriodPage
+        {
+            GracePeriod = 0
+        };
+
+        // Act
+        var result = page.CanShowListOfMemberDetails;
+
+        // Assert
+        Assert.True(result);
+    }
+}
