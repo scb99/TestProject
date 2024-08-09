17985727b42e5cd7ca40f72b5b90a4371faec78e
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 08:40:25 -0400

    ExpiredMembershipsDatePickerPage.razor.cs tests added

---
+++ Pages Tests/ExpiredMembershipsDatePickerPageTests.cs
@@ -0,0 +1,68 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class ExpiredMembershipsDatePickerPageTests
+{
+    [Fact]
+    public void CanShowListOfExpiredMemberships_ShouldReturnFalse_WhenDatesAreMinValue()
+    {
+        // Arrange
+        var page = new ExpiredMembershipsDatePickerPage();
+
+        // Act
+        var result = page.CanShowListOfExpiredMemberships;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowListOfExpiredMemberships_ShouldReturnTrue_WhenDatesAreSet()
+    {
+        // Arrange
+        var page = new ExpiredMembershipsDatePickerPage
+        {
+            StartDateBDP = new DateTime(2023, 1, 1),
+            EndDateBDP = new DateTime(2023, 12, 31)
+        };
+
+        // Act
+        var result = page.CanShowListOfExpiredMemberships;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowListOfExpiredMemberships_ShouldReturnFalse_WhenOnlyStartDateIsSet()
+    {
+        // Arrange
+        var page = new ExpiredMembershipsDatePickerPage
+        {
+            StartDateBDP = new DateTime(2023, 1, 1)
+        };
+
+        // Act
+        var result = page.CanShowListOfExpiredMemberships;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowListOfExpiredMemberships_ShouldReturnFalse_WhenOnlyEndDateIsSet()
+    {
+        // Arrange
+        var page = new ExpiredMembershipsDatePickerPage
+        {
+            EndDateBDP = new DateTime(2023, 12, 31)
+        };
+
+        // Act
+        var result = page.CanShowListOfExpiredMemberships;
+
+        // Assert
+        Assert.False(result);
+    }
+}
