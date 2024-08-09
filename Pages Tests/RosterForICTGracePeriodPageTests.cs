164d6ebf409f2de66065954aeeff866206d98a5e
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 10:46:25 -0400

    RosterForICTGracePeriodPage tests

---
+++ Pages Tests/RosterForICTGracePeriodPageTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class RosterForICTGracePeriodPageTests
+{
+    [Fact]
+    public void CanShowRoster_ShouldReturnFalse_WhenGracePeriodIsMinusOne()
+    {
+        // Arrange
+        var page = new RosterForICTGracePeriodPage();
+
+        // Act
+        var result = page.CanShowRoster;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowRoster_ShouldReturnTrue_WhenGracePeriodIsSet()
+    {
+        // Arrange
+        var page = new RosterForICTGracePeriodPage
+        {
+            GracePeriod = 10
+        };
+
+        // Act
+        var result = page.CanShowRoster;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowRoster_ShouldReturnTrue_WhenGracePeriodIsZero()
+    {
+        // Arrange
+        var page = new RosterForICTGracePeriodPage
+        {
+            GracePeriod = 0
+        };
+
+        // Act
+        var result = page.CanShowRoster;
+
+        // Assert
+        Assert.True(result);
+    }
+}
