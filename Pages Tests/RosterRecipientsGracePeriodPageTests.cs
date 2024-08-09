0aac193cc3a765e9c9c1af626071016d0f481a1f
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 10:43:17 -0400

    RosterRecipientsGracePeriodPageTests added

---
+++ Pages Tests/RosterRecipientsGracePeriodPageTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class RosterRecipientsGracePeriodPageTests
+{
+    [Fact]
+    public void CanShowRosterRecipientsComponent_ShouldReturnFalse_WhenGracePeriodIsMinusOne()
+    {
+        // Arrange
+        var page = new RosterRecipientsGracePeriodPage();
+
+        // Act
+        var result = page.CanShowRosterRecipientsComponent;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowRosterRecipientsComponent_ShouldReturnTrue_WhenGracePeriodIsSet()
+    {
+        // Arrange
+        var page = new RosterRecipientsGracePeriodPage
+        {
+            GracePeriod = 10
+        };
+
+        // Act
+        var result = page.CanShowRosterRecipientsComponent;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowRosterRecipientsComponent_ShouldReturnTrue_WhenGracePeriodIsZero()
+    {
+        // Arrange
+        var page = new RosterRecipientsGracePeriodPage
+        {
+            GracePeriod = 0
+        };
+
+        // Act
+        var result = page.CanShowRosterRecipientsComponent;
+
+        // Assert
+        Assert.True(result);
+    }
+}
