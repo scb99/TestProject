1bf9535481df95fd8f3578f917f382f43bd83166
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 08:53:46 -0400

    Added MembesWithoutEmailGracePeriodPage and SoonToExpireMembshipsDatePickerPage tests

---
+++ Pages Tests/MembersWithoutEmailGracePeriodPageTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class MembersWithoutEmailGracePeriodPageTests
+{
+    [Fact]
+    public void CanShowListOfMembersWithoutEmailAddresses_ShouldReturnFalse_WhenGracePeriodIsMinusOne()
+    {
+        // Arrange
+        var page = new MembersWithoutEmailGracePeriodPage();
+
+        // Act
+        var result = page.CanShowListOfMembersWithoutEmailAddresses;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowListOfMembersWithoutEmailAddresses_ShouldReturnTrue_WhenGracePeriodIsSet()
+    {
+        // Arrange
+        var page = new MembersWithoutEmailGracePeriodPage
+        {
+            GracePeriod = 10
+        };
+
+        // Act
+        var result = page.CanShowListOfMembersWithoutEmailAddresses;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowListOfMembersWithoutEmailAddresses_ShouldReturnTrue_WhenGracePeriodIsZero()
+    {
+        // Arrange
+        var page = new MembersWithoutEmailGracePeriodPage
+        {
+            GracePeriod = 0
+        };
+
+        // Act
+        var result = page.CanShowListOfMembersWithoutEmailAddresses;
+
+        // Assert
+        Assert.True(result);
+    }
+}
---
+++ Pages Tests/SoonToExpireMembershipsDatePickerPageTests.cs
@@ -0,0 +1,35 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class SoonToExpireMembershipsDatePickerPageTests
+{
+    [Fact]
+    public void CanShowSoonToExpireMemberships_ShouldReturnFalse_WhenEndDateIsMinValue()
+    {
+        // Arrange
+        var page = new SoonToExpireMembershipsDatePickerPage();
+
+        // Act
+        var result = page.CanShowSoonToExpireMemberships;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowSoonToExpireMemberships_ShouldReturnTrue_WhenEndDateIsSet()
+    {
+        // Arrange
+        var page = new SoonToExpireMembershipsDatePickerPage
+        {
+            EndDate = new DateTime(2023, 12, 31)
+        };
+
+        // Act
+        var result = page.CanShowSoonToExpireMemberships;
+
+        // Assert
+        Assert.True(result);
+    }
+}
