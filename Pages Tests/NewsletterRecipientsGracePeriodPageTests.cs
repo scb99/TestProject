e06180a9d76353ed1702088eb603c2cd39118f35
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-09 10:38:20 -0400

    NewsletterRecipientsGracePeriodPage tests

---
+++ Pages Tests/NewsletterRecipientsGracePeriodPageTests.cs
@@ -0,0 +1,51 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class NewsletterRecipientsGracePeriodPageTests
+{
+    [Fact]
+    public void CanShowNewsletterRecipients_ShouldReturnFalse_WhenGracePeriodIsMinusOne()
+    {
+        // Arrange
+        var page = new NewsletterRecipientsGracePeriodPage();
+
+        // Act
+        var result = page.CanShowNewsletterRecipients;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowNewsletterRecipients_ShouldReturnTrue_WhenGracePeriodIsSet()
+    {
+        // Arrange
+        var page = new NewsletterRecipientsGracePeriodPage
+        {
+            GracePeriod = 10
+        };
+
+        // Act
+        var result = page.CanShowNewsletterRecipients;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowNewsletterRecipients_ShouldReturnTrue_WhenGracePeriodIsZero()
+    {
+        // Arrange
+        var page = new NewsletterRecipientsGracePeriodPage
+        {
+            GracePeriod = 0
+        };
+
+        // Act
+        var result = page.CanShowNewsletterRecipients;
+
+        // Assert
+        Assert.True(result);
+    }
+}
