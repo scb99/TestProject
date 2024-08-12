d49c3e4357939f2e6ce2ef5a59e1c4d073c2f91f
Steve Bruell <bruell@cs.uiowa.edu>
2024-08-12 15:58:25 -0400

    Add tests for RemoveAllFromPaymentsComponents.razor.cs and RemoveAllPaymentsServiceTests.cs

---
+++ Service Tests/Member Payment Tests/RemoveAllFromPaymentsComponentTests.cs
@@ -0,0 +1,72 @@
+﻿using DataAccess.Models;
+using DBExplorerBlazor.Components;
+using DBExplorerBlazor.Interfaces;
+using Moq;
+using System.Collections.ObjectModel;
+
+namespace MemberPayment;
+
+public class RemoveAllFromPaymentsComponentTests
+{
+    private readonly Mock<ICrossCuttingLoggerService> _loggerMock;
+    private readonly Mock<ICrossCuttingPaymentsService> _paymentsServiceMock;
+    private readonly Mock<IRemoveAllPaymentsService> _removeAllPaymentsServiceMock;
+    private readonly RemoveAllFromPaymentsComponent _component;
+
+    public RemoveAllFromPaymentsComponentTests()
+    {
+        _loggerMock = new Mock<ICrossCuttingLoggerService>();
+        _paymentsServiceMock = new Mock<ICrossCuttingPaymentsService>();
+        _removeAllPaymentsServiceMock = new Mock<IRemoveAllPaymentsService>();
+        _component = new RemoveAllFromPaymentsComponent
+        {
+            Logger = _loggerMock.Object,
+            PaymentsService = _paymentsServiceMock.Object,
+            RemoveAllPaymentsService = _removeAllPaymentsServiceMock.Object
+        };
+    }
+
+    [Fact]
+    public void OnInitialized_SubscribesToPaymentEntitiesSizeChanged()
+    {
+        // Act
+        _component.OnInitialized2();
+
+        // Assert
+        _paymentsServiceMock.VerifyAdd(p => p.PaymentEntitiesSizeChanged += It.IsAny<Action>(), Times.Once);
+    }
+
+    [Fact]
+    public void OnPaymentEntitiesSizeChanged_UpdatesDisabledBDP()
+    {
+        // Arrange
+        ObservableCollection<PaymentEntity> temp = new();
+        _paymentsServiceMock.Setup(p => p.PaymentEntities).Returns(temp);
+
+        // Act
+        _component.OnPaymentEntitiesSizeChanged2();
+
+        // Assert
+        Assert.True(_component.DisabledBDP);
+    }
+
+    [Fact]
+    public async Task OnRemoveAllButtonClickedAsync_CallsRemoveAllPaymentsAsync()
+    {
+        // Act
+        await _component.OnRemoveAllButtonClickedAsync();
+
+        // Assert
+        _removeAllPaymentsServiceMock.Verify(r => r.RemoveAllPaymentsAsync(), Times.Once);
+    }
+
+    [Fact]
+    public void Dispose_UnsubscribesFromPaymentEntitiesSizeChanged()
+    {
+        // Act
+        _component.Dispose();
+
+        // Assert
+        _paymentsServiceMock.VerifyRemove(p => p.PaymentEntitiesSizeChanged -= It.IsAny<Action>(), Times.Once);
+    }
+}
---
+++ Service Tests/Member Payment Tests/RemoveAllPaymentsServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using DBExplorerBlazor.Interfaces;
+using DBExplorerBlazor.Services;
+using Moq;
+
+namespace MemberPayment;
+
+public class RemoveAllPaymentsServiceTests
+{
+    private readonly Mock<ICrossCuttingPaymentsService> _paymentsServiceMock;
+    private readonly Mock<ILoggerService> _loggerMock;
+    private readonly RemoveAllPaymentsService _removeAllPaymentsService;
+
+    public RemoveAllPaymentsServiceTests()
+    {
+        _paymentsServiceMock = new Mock<ICrossCuttingPaymentsService>();
+        _loggerMock = new Mock<ILoggerService>();
+        _removeAllPaymentsService = new RemoveAllPaymentsService(_paymentsServiceMock.Object, _loggerMock.Object);
+    }
+
+    [Fact]
+    public async Task RemoveAllPaymentsAsync_CallsRemoveAllPaymentsFromPaymentEntities()
+    {
+        // Act
+        await _removeAllPaymentsService.RemoveAllPaymentsAsync();
+
+        // Assert
+        _paymentsServiceMock.Verify(p => p.RemoveAllPaymentsFromPaymentEntities(), Times.Once);
+    }
+
+    [Fact]
+    public async Task RemoveAllPaymentsAsync_LogsException_WhenExceptionIsThrown()
+    {
+        // Arrange
+        var exception = new Exception("Test exception");
+        _paymentsServiceMock.Setup(p => p.RemoveAllPaymentsFromPaymentEntities()).Throws(exception);
+
+        // Act
+        await _removeAllPaymentsService.RemoveAllPaymentsAsync();
+
+        // Assert
+        _loggerMock.Verify(l => l.LogExceptionAsync(exception, nameof(RemoveAllPaymentsService)), Times.Once);
+    }
+}
