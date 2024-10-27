698d73029351a03c4a7aa45f49a2d5c957a6f870
Steve Bruell <bruell@cs.uiowa.edu>
2024-10-27 17:22:59 -0400

    Cleanup MemberDetailsAHelperTests.cs

--- Components/Menu Item Components/Members/Tabs/Member Details Components/MemberDetailsAHelperTests.cs
+++ Components/Menu Item Components/Members/Tabs/Member Details Components/MemberDetailsAHelperTests.cs
@@ -5,13 +5,13 @@ using Moq;
 
 namespace MenuItemComponents;
 
-public class MemberDetailsHelperTests
+public class MemberDetailsAHelperTests
 {
     [Fact]
     public void GetDisplayNames_ReturnsCorrectDisplayNames()
     {
         // Act
-        var displayNames = MemberDetailsHelper.GetDisplayNames();
+        var displayNames = MemberDetailsAHelper.GetDisplayNames();
 
         // Assert
         Assert.Equal(16, displayNames.Length);
@@ -34,7 +34,7 @@ public class MemberDetailsHelperTests
         mockService.Setup(s => s.MemberDetailEntities).Returns(memberDetailEntities);
 
         // Act
-        var result = MemberDetailsHelper.GetMemberDetailEntities(displayNames, mockService.Object);
+        var result = MemberDetailsAHelper.GetMemberDetailEntities(displayNames, mockService.Object);
 
         // Assert
         Assert.Equal(2, result.Count);
@@ -53,31 +53,9 @@ public class MemberDetailsHelperTests
         };
 
         // Act
-        var memberName = MemberDetailsHelper.GetMemberName(memberDetailEntities);
+        var memberName = MemberDetailsAHelper.GetMemberName(memberDetailEntities);
 
         // Assert
         Assert.Equal("Doe John", memberName);
     }
-
-    //[Fact]
-    //public void CustomizeCell_SetsCorrectCellClass()
-    //{
-    //    // Arrange
-    //    var args = new QueryCellInfoEventArgs<MemberDetailEntity>
-    //    {
-    //        Column = new Column { Field = "Value" },
-    //        Data = new MemberDetailEntity { DisplayName = "First Name", Value = "John" },
-    //        Cell = new Cell()
-    //    };
-
-    //    // Mock the validator
-    //    var mockValidator = new Mock<MemberDetailsValidators>();
-    //    mockValidator.Setup(v => v.IsMemberDetailsADataValid(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
-
-    //    // Act
-    //    MemberDetailsHelper.CustomizeCell(args);
-
-    //    // Assert
-    //    Assert.Contains("noerror", args.Cell.CssClass);
-    //}
 }
