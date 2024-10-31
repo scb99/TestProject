3dbab3586aade9ddbd7eabc042409994700652cc
Steve Bruell <bruell@cs.uiowa.edu>
2024-10-31 10:27:31 -0400

    Added MemberDetailsTabItemComponentTests.cs

---
+++ Components/Menu Item Components/Members/Tabs/Member Details Tab Item Components/MemberDetailsTabItemComponentTests.cs
@@ -0,0 +1,34 @@
+﻿using DBExplorerBlazor.Components;
+using DBExplorerBlazor.Interfaces;
+using Moq;
+
+namespace MenuItemComponents;
+
+public class MemberDetailsTabItemComponentTests
+{
+    private readonly Mock<ICrossCuttingMemberIDService> _mockMemberIDService;
+    private readonly MemberDetailsTabItemComponent _component;
+
+    public MemberDetailsTabItemComponentTests()
+    {
+        _mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
+        _component = new MemberDetailsTabItemComponent
+        {
+            MemberIDService = _mockMemberIDService.Object
+        };
+    }
+
+    [Fact]
+    public void MemberIDService_IsInjected()
+    {
+        // Arrange & Act
+        var memberIDServiceField = _component.GetType().GetProperty("MemberIDService"); //, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var memberIDServiceValue = memberIDServiceField?.GetValue(_component);
+
+        // Assert
+        Assert.NotNull(memberIDServiceValue);
+        Assert.IsAssignableFrom<ICrossCuttingMemberIDService>(memberIDServiceValue);
+    }
+
+    // Additional tests can be added here to verify other behaviors of the component
+}
