9964c374c9c37b773690b0bb02abc326391e09c6
Steve Bruell <bruell@cs.uiowa.edu>
2024-10-31 10:42:27 -0400

    Added MemberPaymentHistoryTabItemComponentTests.cs

---
+++ Components/Menu Item Components/Members/Tabs/Member Payment History Tab Item Components/MemberPaymentHistoryTabItemComponentTests.cs
@@ -0,0 +1,28 @@
+﻿using DBExplorerBlazor.Components;
+using DBExplorerBlazor.Interfaces;
+using Moq;
+
+namespace MenuItemComponents;
+
+public class MemberPaymentHistoryTabItemComponentTests
+{
+    [Fact]
+    public void MemberPaymentHistoryTabItemComponent_ShouldInjectMemberIDService()
+    {
+        // Arrange
+        var mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
+        var component = new MemberPaymentHistoryTabItemComponent
+        {
+            MemberIDService = mockMemberIDService.Object
+        };
+
+        // Act
+        var memberIDServiceField = component.GetType().GetProperty("MemberIDService"); //, System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var memberIDServiceValue = memberIDServiceField?.GetValue(component);
+
+        // Assert
+        Assert.NotNull(memberIDServiceValue);
+        Assert.IsAssignableFrom<ICrossCuttingMemberIDService>(memberIDServiceValue);
+        Assert.Equal(mockMemberIDService.Object, memberIDServiceValue);
+    }
+}
