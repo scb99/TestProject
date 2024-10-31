d0bf1da41c139bf1e8115b3e077cc186e7d57286
Steve Bruell <bruell@cs.uiowa.edu>
2024-10-31 11:53:48 -0400

    Added MemberSubscriptionsOrdersTabItemComponentTests.cs

---
+++ Components/Menu Item Components/Members/Tabs/Member Subscriptions Orders Tab Item Components/MemberSubscriptionsOrdersTabItemComponentTests.cs
@@ -0,0 +1,27 @@
+﻿using DBExplorerBlazor.Components;
+using DBExplorerBlazor.Interfaces;
+using Moq;
+
+namespace MenuItemComponents;
+
+public class MemberSubscriptionsOrdersTabItemComponentTests
+{
+    [Fact]
+    public void MemberSubscriptionsOrdersTabItemComponent_ShouldInjectMemberIDService()
+    {
+        // Arrange
+        var mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
+        var component = new MemberSubscriptionsOrdersTabItemComponent
+        {
+            MemberIDService = mockMemberIDService.Object
+        };
+
+        // Act
+        var memberIDServiceField = component.GetType().GetProperty("MemberIDService"); //, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
+        var memberIDServiceValue = memberIDServiceField?.GetValue(component);
+
+        // Assert
+        Assert.NotNull(memberIDServiceValue);
+        Assert.Equal(mockMemberIDService.Object, memberIDServiceValue);
+    }
+}
