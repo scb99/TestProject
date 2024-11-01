6f8a1d00a75e36986bc72f8462b43c93a60e4a74
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-01 17:09:46 -0400

    Added DescriptionOfResultComponentTests.cs

---
+++ Components/Shared Components/DescriptionOfResultComponentTests.cs
@@ -0,0 +1,47 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class DescriptionOfResultComponentTests
+{
+    [Fact]
+    public void Title_ShouldBeEmptyStringByDefault()
+    {
+        // Arrange
+        var component = new DescriptionOfResultComponent();
+
+        // Act
+        var title = component.Title;
+
+        // Assert
+        Assert.Equal(string.Empty, title);
+    }
+
+    [Fact]
+    public void DisplayTitle_ShouldBeFalseWhenTitleIsEmpty()
+    {
+        // Arrange
+        var component = new DescriptionOfResultComponent();
+        component.Initialize(string.Empty);
+
+        // Act
+        var displayTitle = component.GetType().GetProperty("DisplayTitle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(component);
+
+        // Assert
+        Assert.False(displayTitle is bool displayTitleValue && displayTitleValue);
+    }
+
+    [Fact]
+    public void DisplayTitle_ShouldBeTrueWhenTitleIsNotEmpty()
+    {
+        // Arrange
+        var component = new DescriptionOfResultComponent();
+        component.Initialize("Test Title");
+
+        // Act
+        var displayTitle = component.GetType().GetProperty("DisplayTitle", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)?.GetValue(component);
+
+        // Assert
+        Assert.True(displayTitle is bool displayTitleValue && displayTitleValue);
+    }
+}
