fd1ab521be7e81816ce36a82d311983498ffb82e
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-02 14:15:55 -0400

    Added DownloadButtonReferenceComponentTests.cs

---
+++ Components/Shared Components/DownloadButtonReferenceComponentTests.cs
@@ -0,0 +1,48 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class DownloadButtonReferenceComponentTests
+{
+    [Fact]
+    public void Text_ShouldBeNullByDefault()
+    {
+        // Arrange
+        var component = new DownloadButtonReferenceComponent();
+
+        // Act
+        var text = component.Text;
+
+        // Assert
+        Assert.Null(text);
+    }
+
+    [Fact]
+    public void Style_ShouldBeNullByDefault()
+    {
+        // Arrange
+        var component = new DownloadButtonReferenceComponent();
+
+        // Act
+        var style = component.Style;
+
+        // Assert
+        Assert.Null(style);
+    }
+
+    [Fact]
+    public void Initialize_ShouldSetTextAndStyle()
+    {
+        // Arrange
+        var component = new DownloadButtonReferenceComponent();
+        var expectedText = "Download";
+        var expectedStyle = "btn-primary";
+
+        // Act
+        component.Initialize(expectedText, expectedStyle);
+
+        // Assert
+        Assert.Equal(expectedText, component.Text);
+        Assert.Equal(expectedStyle, component.Style);
+    }
+}
