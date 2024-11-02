ca94529d75942f42201a606f8deff43a5f2dac10
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-02 14:19:39 -0400

    Added DownloadCSVFileComponentTests.cs

---
+++ Components/Shared Components/DownloadCSVFileComponentTests.cs
@@ -0,0 +1,19 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class DownloadCSVFileComponentTests
+{
+    [Fact]
+    public void OnClick_ShouldBeDefaultEventCallback()
+    {
+        // Arrange
+        var component = new DownloadCSVFileComponent();
+
+        // Act
+        var onClick = component.OnClick;
+
+        // Assert
+        Assert.False(onClick.HasDelegate);
+    }
+}
