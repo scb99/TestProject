db9626cbb512457a8375fb5333c9a7ef3d716b88
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-02 14:27:38 -0400

    Added DownloadTXTFileComponentTests.cs

---
+++ Components/Shared Components/Class1.cs
@@ -0,0 +1,19 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class DownloadTXTFileComponentTests
+{
+    [Fact]
+    public void OnClick_ShouldBeDefaultEventCallback()
+    {
+        // Arrange
+        var component = new DownloadTXTFileComponent();
+
+        // Act
+        var onClick = component.OnClick;
+
+        // Assert
+        Assert.False(onClick.HasDelegate);
+    }
+}
