22d3c02e33e2a0cf11a2a05b627c0ceb08df2f54
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-02 14:24:05 -0400

    Added DownloadExcelFileComponentTests.cs

---
+++ Components/Shared Components/DownloadExcelFileComponentTests.cs
@@ -0,0 +1,19 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class DownloadExcelFileComponentTests
+{
+    [Fact]
+    public void OnClick_ShouldBeDefaultEventCallback()
+    {
+        // Arrange
+        var component = new DownloadExcelFileComponent();
+
+        // Act
+        var onClick = component.OnClick;
+
+        // Assert
+        Assert.False(onClick.HasDelegate);
+    }
+}
