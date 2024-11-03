2ce6fe77fa7918474501f8138c717d5ff79239b0
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-03 12:42:56 -0500

    Moved files and folders

---
+++ Pages/Menu Item Pages/Logs1/LogsDatePickerPageTests.cs
@@ -0,0 +1,74 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace MenuItemPages;
+
+public class LogsDatePickerPageTests
+{
+    [Fact]
+    public void CanShowLogs_WhenBothDatesAreMinValue_ReturnsFalse()
+    {
+        // Arrange
+        var page = new LogsDatePickerPage
+        {
+            EndDateBDP = DateTime.MinValue,
+            StartDateBDP = DateTime.MinValue
+        };
+
+        // Act
+        var result = page.CanShowLogs;
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanShowLogs_WhenEndDateIsNotMinValue_ReturnsTrue()
+    {
+        // Arrange
+        var page = new LogsDatePickerPage
+        {
+            EndDateBDP = DateTime.Now,
+            StartDateBDP = DateTime.MinValue
+        };
+
+        // Act
+        var result = page.CanShowLogs;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowLogs_WhenStartDateIsNotMinValue_ReturnsTrue()
+    {
+        // Arrange
+        var page = new LogsDatePickerPage
+        {
+            EndDateBDP = DateTime.MinValue,
+            StartDateBDP = DateTime.Now
+        };
+
+        // Act
+        var result = page.CanShowLogs;
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanShowLogs_WhenBothDatesAreNotMinValue_ReturnsTrue()
+    {
+        // Arrange
+        var page = new LogsDatePickerPage
+        {
+            EndDateBDP = DateTime.Now,
+            StartDateBDP = DateTime.Now
+        };
+
+        // Act
+        var result = page.CanShowLogs;
+
+        // Assert
+        Assert.True(result);
+    }
+}
