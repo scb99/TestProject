92f0d6fe28ace529e03f000f373b231e1283356e
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-03 10:46:00 -0500

    Added RightArrowComponentTests.cs

---
+++ Components/Shared Components/RightArrowComponentTests.cs
@@ -0,0 +1,19 @@
+﻿using DBExplorerBlazor.Components;
+
+namespace SharedComponents;
+
+public class RightArrowComponentTests
+{
+    [Fact]
+    public void RightArrowComponent_Initialization_Test()
+    {
+        // Arrange
+        var rightArrowComponent = new RightArrowComponent();
+
+        // Act
+        // No specific action needed as we are testing initialization
+
+        // Assert
+        Assert.NotNull(rightArrowComponent);
+    }
+}
