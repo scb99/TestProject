8003845d3774dd25f80f9b904c08a561280ee2de
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-03 11:23:21 -0500

    Added LogoutPageTests.cs

---
+++ Components/Menu Item Components/Logout/LogoutPageTests.cs
@@ -0,0 +1,40 @@
+﻿using DBExplorerBlazor.Interfaces;
+using DBExplorerBlazor.Pages;
+using Moq;
+
+namespace MenuItemComponents;
+
+public class LogoutPageTests
+{
+    private readonly Mock<ILogoutService> _mockLogoutService;
+    private readonly LogoutPage _logoutPage;
+
+    public LogoutPageTests()
+    {
+        _mockLogoutService = new Mock<ILogoutService>();
+        _logoutPage = new LogoutPage
+        {
+            LogoutService = _mockLogoutService.Object
+        };
+    }
+
+    [Fact]
+    public async Task OnYesLogoutButtonClickedAsync_CallsHandleLogoutAsync()
+    {
+        // Act
+        await _logoutPage.OnYesLogoutButtonClickedAsync();
+
+        // Assert
+        _mockLogoutService.Verify(service => service.HandleLogoutAsync(), Times.Once);
+    }
+
+    [Fact]
+    public async Task OnNoLogoutButtonClickedAsync_CallsHandleCancelLogoutAsync()
+    {
+        // Act
+        await _logoutPage.OnNoLogoutButtonClickedAsync();
+
+        // Assert
+        _mockLogoutService.Verify(service => service.HandleCancelLogoutAsync(), Times.Once);
+    }
+}
