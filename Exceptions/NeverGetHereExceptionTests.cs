701d370ac5503006d4aacc3cce2843bde27dd376
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-05 10:14:10 -0500

    Added NeverGetHereExceptionTests.cs

---
+++ Exceptions/NeverGetHereExceptionTests.cs
@@ -0,0 +1,42 @@
+﻿using DBExplorerBlazor;
+
+namespace Exceptions;
+
+public class NeverGetHereExceptionTests
+{
+    [Fact]
+    public void NeverGetHereException_ShouldInheritFromException()
+    {
+        // Arrange & Act
+        var exception = new NeverGetHereException("Test message");
+
+        // Assert
+        Assert.IsAssignableFrom<Exception>(exception);
+    }
+
+    [Fact]
+    public void NeverGetHereException_ShouldSetMessageProperty()
+    {
+        // Arrange
+        var testMessage = "Test message";
+
+        // Act
+        var exception = new NeverGetHereException(testMessage);
+
+        // Assert
+        Assert.Equal(testMessage, exception.Message);
+    }
+
+    [Fact]
+    public void NeverGetHereException_ShouldBeSerializable()
+    {
+        // Arrange
+        var exception = new NeverGetHereException("Test message");
+
+        // Act
+        var isSerializable = exception.GetType().IsSerializable;
+
+        // Assert
+        Assert.True(isSerializable);
+    }
+}
