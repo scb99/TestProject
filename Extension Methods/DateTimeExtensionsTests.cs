5a2f398cb3a9d4debc01bc6fff49dc41ac3afc3d
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-05 10:20:52 -0500

    Added DateTimeExtensionsTests.cs

---
+++ Extension Methods/DateTimeExtensionsTests.cs
@@ -0,0 +1,58 @@
+﻿using DBExplorerBlazor;
+
+namespace ExtensionMethods;
+
+public class DateTimeExtensionsTests
+{
+    [Fact]
+    public void ConvertToStringDate_ReturnsCorrectFormat()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 10, 5);
+
+        // Act
+        var result = dateTime.ConvertToStringDate();
+
+        // Assert
+        Assert.Equal("2023-10-05", result);
+    }
+
+    [Fact]
+    public void ConvertToStringDate_HandlesSingleDigitMonthAndDay()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 1, 9);
+
+        // Act
+        var result = dateTime.ConvertToStringDate();
+
+        // Assert
+        Assert.Equal("2023-01-09", result);
+    }
+
+    [Fact]
+    public void ConvertToStringDate_HandlesLeapYear()
+    {
+        // Arrange
+        var dateTime = new DateTime(2020, 2, 29);
+
+        // Act
+        var result = dateTime.ConvertToStringDate();
+
+        // Assert
+        Assert.Equal("2020-02-29", result);
+    }
+
+    [Fact]
+    public void ConvertToStringDate_HandlesEndOfYear()
+    {
+        // Arrange
+        var dateTime = new DateTime(2023, 12, 31);
+
+        // Act
+        var result = dateTime.ConvertToStringDate();
+
+        // Assert
+        Assert.Equal("2023-12-31", result);
+    }
+}
