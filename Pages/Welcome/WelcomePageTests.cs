e5fb02fc15e2adbb7778100d6dc5764e8d0ebf9b
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-12 15:11:21 -0500

    Added WelcomePageTests.cs

---
+++ Pages/Welcome/WelcomePageTests.cs
@@ -0,0 +1,16 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace Pages;
+
+public class WelcomePageTests
+{
+    [Fact]
+    public void WelcomePage_CanBeInstantiated()
+    {
+        // Arrange & Act
+        var page = new WelcomePage();
+
+        // Assert
+        Assert.NotNull(page);
+    }
+}
