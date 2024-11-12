66fc771d31a68b39f9810badf59644569b4b0811
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-12 15:04:03 -0500

    Added TotalMembersChartPageTests.cs

---
+++ Pages/Menu Item Pages/Website/TotalMembersChartPageTests.cs
@@ -0,0 +1,17 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace MenuItemPages
+{
+    public class TotalMembersChartPageTests
+    {
+        [Fact]
+        public void TotalMembersChartPage_CanBeInstantiated()
+        {
+            // Arrange & Act
+            var page = new TotalMembersChartPage();
+
+            // Assert
+            Assert.NotNull(page);
+        }
+    }
+}
