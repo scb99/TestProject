563b8a042533a0dd0f1469b0750cc7227f264438
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-12 15:01:56 -0500

    Added TotalAssetsChartPageTests.cs

---
+++ Pages/Menu Item Pages/Website/TotalAssetsChartPageTests.cs
@@ -0,0 +1,17 @@
+﻿using DBExplorerBlazor.Pages;
+
+namespace MenuItemPages
+{
+    public class TotalAssetsChartPageTests
+    {
+        [Fact]
+        public void TotalAssetsChartPage_CanBeInstantiated()
+        {
+            // Arrange & Act
+            var page = new TotalAssetsChartPage();
+
+            // Assert
+            Assert.NotNull(page);
+        }
+    }
+}
