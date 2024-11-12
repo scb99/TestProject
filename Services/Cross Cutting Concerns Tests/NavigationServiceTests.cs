e85250f17b43d264b0ab8ca16dfb963d67bf4531
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-12 15:56:45 -0500

    Added NavigationServiceTests.cs

---
+++ Services/Cross Cutting Concerns Tests/NavigationServiceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace CrossCuttingConcerns;
+
+public class NavigationServiceTests
+{
+    //[Fact]
+    //public void NavigateTo_CallsNavigationManagerNavigateTo()
+    //{
+    //    // Arrange
+    //    var mockNavigationManager = new Mock<NavigationManager>();
+    //    var navigationService = new NavigationService(mockNavigationManager.Object);
+    //    var url = "http://example.com";
+
+    //    // Act
+    //    navigationService.NavigateTo(url);
+
+    //    // Assert
+    //    mockNavigationManager.Verify(nm => nm.NavigateTo(url, It.IsAny<bool>()), Times.Once);
+    //}
+}
