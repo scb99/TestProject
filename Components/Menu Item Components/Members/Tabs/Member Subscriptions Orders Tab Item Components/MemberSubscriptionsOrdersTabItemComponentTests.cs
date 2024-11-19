d5577be14488d12fd5f193ed8929f2be4b904084
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-19 14:22:54 -0500

    Modified MemberSubscriptionsOrdersTabItemComponentTests.cs to use private data members

--- Components/Menu Item Components/Members/Tabs/Member Subscriptions Orders Tab Item Components/MemberSubscriptionsOrdersTabItemComponentTests.cs
+++ Components/Menu Item Components/Members/Tabs/Member Subscriptions Orders Tab Item Components/MemberSubscriptionsOrdersTabItemComponentTests.cs
@@ -1,27 +1,33 @@
 ﻿using DBExplorerBlazor.Components;
 using DBExplorerBlazor.Interfaces;
+using ExtensionMethods;
 using Moq;
 
 namespace MenuItemComponents;
 
 public class MemberSubscriptionsOrdersTabItemComponentTests
 {
+    private readonly Mock<ICrossCuttingMemberIDService> _mockMemberIDService;
+    private readonly MemberSubscriptionsOrdersTabItemComponent _component;
+
+    public MemberSubscriptionsOrdersTabItemComponentTests()
+    {
+        _mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
+
+        _component = new MemberSubscriptionsOrdersTabItemComponent();
+
+        _component.SetPrivatePropertyValue("MemberIDService", _mockMemberIDService.Object);
+    }
+
     [Fact]
     public void MemberSubscriptionsOrdersTabItemComponent_ShouldInjectMemberIDService()
     {
-        // Arrange
-        var mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
-        var component = new MemberSubscriptionsOrdersTabItemComponent
-        {
-            MemberIDService = mockMemberIDService.Object
-        };
-
-        // Act
-        var memberIDServiceField = component.GetType().GetProperty("MemberIDService"); //, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
-        var memberIDServiceValue = memberIDServiceField?.GetValue(component);
+        // Arrange and Act
+        var injectedService = _component.GetPrivatePropertyValue<ICrossCuttingMemberIDService>("MemberIDService"); // as ICrossCuttingMemberIDService;
 
         // Assert
-        Assert.NotNull(memberIDServiceValue);
-        Assert.Equal(mockMemberIDService.Object, memberIDServiceValue);
+        Assert.NotNull(injectedService);
+        Assert.IsAssignableFrom<ICrossCuttingMemberIDService>(injectedService);
+        Assert.Equal(_mockMemberIDService.Object, injectedService);
     }
 }
