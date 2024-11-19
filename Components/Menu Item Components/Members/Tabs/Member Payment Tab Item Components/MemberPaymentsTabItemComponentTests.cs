56551a09151264c06d7fba08eec00ac96578d6aa
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-19 09:46:31 -0500

    Modified MemberPaymentsTabItemComponentTests.cs to use private data members

--- Components/Menu Item Components/Members/Tabs/Member Payment Tab Item Components/MemberPaymentsTabItemComponentTests.cs
+++ Components/Menu Item Components/Members/Tabs/Member Payment Tab Item Components/MemberPaymentsTabItemComponentTests.cs
@@ -25,21 +25,8 @@ public class MemberPaymentsTabItemComponentTests
         // Arrange and Act
         var injectedService = _component.GetPrivatePropertyValue<ICrossCuttingMemberIDService>("MemberIDService"); // as ICrossCuttingMemberIDService;
 
-        // Arrange
-        //var mockMemberIDService = new Mock<ICrossCuttingMemberIDService>();
-        //var component = new MemberPaymentsTabItemComponent
-        //{
-        //    MemberIDService = mockMemberIDService.Object
-        //};
-
-        // Act
-        //var memberIDServiceField = _component.GetType().GetProperty("MemberIDService"); //, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance);
-        //var memberIDServiceValue = memberIDServiceField?.GetValue(_component);
-
         // Assert
         Assert.NotNull(injectedService);
         Assert.Equal(_mockMemberIDService.Object, injectedService);
-        //Assert.NotNull(memberIDServiceValue);
-        //Assert.Equal(_mockMemberIDService.Object, memberIDServiceValue);
     }
 }
