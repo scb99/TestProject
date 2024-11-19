cc8462c6c1ddf9b80a79c2f6f1cf3ed0555dbb5f
Steve Bruell <bruell@cs.uiowa.edu>
2024-11-19 08:48:31 -0500

    Modified AddStripeMemToPaymentsComponentTests.cs to use private data members

--- Components/Menu Item Components/Members/Tabs/Member Payment Stripe Components/AddStripeMemToPaymentsComponentTests.cs
+++ Components/Menu Item Components/Members/Tabs/Member Payment Stripe Components/AddStripeMemToPaymentsComponentTests.cs
@@ -1,32 +1,35 @@
 ﻿using DBExplorerBlazor.Components;
 using DBExplorerBlazor.Interfaces;
+using ExtensionMethods;
 using Moq;
 
 namespace MenuItemComponents;
 
 public class AddStripeMemToPaymentsComponentTests
 {
+    private readonly Mock<ICrossCuttingConditionalCodeService> _mockExecute;
     private readonly Mock<ICrossCuttingStripeService> _mockStripeService;
     private readonly Mock<IStripePaymentHandlerService> _mockStripePaymentHandlerService;
     private readonly AddStripeMemToPaymentsComponent _component;
 
     public AddStripeMemToPaymentsComponentTests()
     {
+        _mockExecute = new Mock<ICrossCuttingConditionalCodeService>();
         _mockStripeService = new Mock<ICrossCuttingStripeService>();
         _mockStripePaymentHandlerService = new Mock<IStripePaymentHandlerService>();
 
-        _component = new AddStripeMemToPaymentsComponent
-        {
-            StripeService = _mockStripeService.Object,
-            StripePaymentHandlerService = _mockStripePaymentHandlerService.Object
-        };
+        _component = new AddStripeMemToPaymentsComponent();
+
+        _component.SetPrivatePropertyValue("Execute", _mockExecute.Object);
+        _component.SetPrivatePropertyValue("StripeService", _mockStripeService.Object);
+        _component.SetPrivatePropertyValue("StripePaymentHandlerService", _mockStripePaymentHandlerService.Object);
     }
 
     [Fact]
     public void OnInitialized_ShouldSubscribeToStripeMemberIDChanged()
     {
         // Act
-        _component.OnInitialized2();
+        typeof(AddStripeMemToPaymentsComponent).Invoke("OnInitialized", _component);
 
         // Assert
         _mockStripeService.VerifyAdd(s => s.StripeMemberIDChanged += It.IsAny<Action<int>>(), Times.Once);
@@ -36,36 +39,42 @@ public class AddStripeMemToPaymentsComponentTests
     public void OnAddButtonClicked_ShouldCallAddStripePayment()
     {
         // Arrange
-        _component._selectedStripeMemberID = 1;
+        _component.SetPrivateMemberValue<int>("_selectedStripeMemberID", 1);
 
         // Act
-        _component.OnAddButtonClicked();
+        typeof(AddStripeMemToPaymentsComponent).Invoke("OnAddButtonClicked", _component);
 
         // Assert
         _mockStripePaymentHandlerService.Verify(s => s.AddStripePayment(1), Times.Once);
     }
 
-    //[Fact]
-    //public void OnStripeMemberIDChange_ShouldUpdateDisabledAndSelectedStripeMemberID()
-    //{
-    //    // Act
-    //    _component.OnStripeMemberIDChange(1);
+    [Fact]
+    public void OnStripeMemberIDChange_ShouldUpdateDisabledAndSelectedStripeMemberID()
+    {
+        // Arrange
+        _mockExecute.Setup(e => e.ConditionalCode()).Returns(false);
 
-    //    // Assert
-    //    Assert.False(_component.Disabled);
-    //    Assert.Equal(1, _component._selectedStripeMemberID);
-    //}
+        // Act
+        typeof(AddStripeMemToPaymentsComponent).Invoke("OnStripeMemberIDChange", _component, 1);
 
-    //[Fact]
-    //public void OnStripeMemberIDChange_ShouldDisableButtonWhenIDIsZero()
-    //{
-    //    // Act
-    //    _component.OnStripeMemberIDChange(0);
+        // Assert
+        Assert.False(_component.GetPrivatePropertyValue<bool>("Disabled"));
+        Assert.Equal(1, _component.GetPrivateMemberValue<int>("_selectedStripeMemberID"));
+    }
 
-    //    // Assert
-    //    Assert.True(_component.Disabled);
-    //    Assert.Equal(0, _component._selectedStripeMemberID);
-    //}
+    [Fact]
+    public void OnStripeMemberIDChange_ShouldDisableButtonWhenIDIsZero()
+    {
+        // Arrange
+        _mockExecute.Setup(e => e.ConditionalCode()).Returns(false);
+
+        // Act
+        typeof(AddStripeMemToPaymentsComponent).Invoke("OnStripeMemberIDChange", _component, 0);
+
+        // Assert
+        Assert.True(_component.GetPrivatePropertyValue<bool>("Disabled"));
+        Assert.Equal(0, _component.GetPrivateMemberValue<int>("_selectedStripeMemberID"));
+    }
 
     [Fact]
     public void Dispose_ShouldUnsubscribeFromStripeMemberIDChanged()
